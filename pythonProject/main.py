703b4b66e29e1246bdc04541b2c5e14f9a3bf90d
Kc <kc@fog.moe>
2024-05-11 21:51:49 +0800

    bug

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -19,38 +19,44 @@ def download_and_repackage():
     user_input = input()
     if user_input.lower() != 'y':
         print("用户取消。")
+        print("请按任意键退出。")
+        input()
         return
 
     # 检查下载目录是否存在
     if not Path(download_folder).exists():
         print(
             f"文件夹 {download_folder} 不存在。 请在 ygopro 目录运行本程序。")
+        print("请按任意键退出。")
+        input()
         return
 
     # 选择下载节点
-    print("请选择下载节点：(输入对应数字)")
-    print("1. GitHub")
-    print("2. KKGitHub")
-    print("3. GHProxy")
-    print("4. GHProxy")
-    print("5. GHProxy")
-    print("6. 备用节点")
-    user_input = input()
-    if user_input == '1':
-        url = 'https://github.com/FogMoe/YGOCustomCards/archive/refs/heads/main.zip'
-    elif user_input == '2':
-        url = 'https://kkgithub.com/FogMoe/YGOCustomCards/archive/refs/heads/main.zip'
-    elif user_input == '3':
-        url = 'https://ghproxy.net/https://github.com/FogMoe/YGOCustomCards/archive/refs/heads/main.zip'
-    elif user_input == '4':
-        url = 'https://mirror.ghproxy.com/https://github.com/FogMoe/YGOCustomCards/archive/refs/heads/main.zip'
-    elif user_input == '5':
-        url = 'https://ghproxy.cc/https://github.com/FogMoe/YGOCustomCards/archive/refs/heads/main.zip'
-    elif user_input == '6':
-        url = 'https://file1.fogmoe.top/YGODiy/YGOCustomCards-main.zip'
-    else:
-        print("无效输入。")
-        return
+    while True:
+        print("请选择下载节点：(输入对应数字)")
+        print("1. GitHub")
+        print("2. KKGitHub")
+        print("3. GHProxy")
+        print("4. GHProxy")
+        print("5. GHProxy")
+        print("6. 备用节点")
+        user_input = input()
+        if user_input == '1':
+            url = 'https://github.com/FogMoe/YGOCustomCards/archive/refs/heads/main.zip'
+        elif user_input == '2':
+            url = 'https://kkgithub.com/FogMoe/YGOCustomCards/archive/refs/heads/main.zip'
+        elif user_input == '3':
+            url = 'https://ghproxy.net/https://github.com/FogMoe/YGOCustomCards/archive/refs/heads/main.zip'
+        elif user_input == '4':
+            url = 'https://mirror.ghproxy.com/https://github.com/FogMoe/YGOCustomCards/archive/refs/heads/main.zip'
+        elif user_input == '5':
+            url = 'https://ghproxy.cc/https://github.com/FogMoe/YGOCustomCards/archive/refs/heads/main.zip'
+        elif user_input == '6':
+            url = 'https://file1.fogmoe.top/YGODiy/YGOCustomCards-main.zip'
+        else:
+            print("无效输入。")
+            continue
+        break
 
     # 下载文件的完整路径
     download_path = os.path.join(download_folder, 'YGOCustomCards-main.zip')
@@ -65,6 +71,8 @@ def download_and_repackage():
         print("下载完毕。")
     except requests.RequestException as e:
         print(f"下载错误： {e}")
+        print("请按任意键退出。")
+        input()
         return
 
     # 解压文件
